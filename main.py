71b8b72c353526195b415c7c41edb97440992518
notkooru <notkooru@gmail.com>
2022-07-23 02:37:59 -0600

    Created Champion class

---
+++ main.py
@@ -0,0 +1,9 @@
+class Champion:
+
+    def __init__(self, name, hasMana, isRanged) -> None:
+        #name = string
+        #hasMana = bool
+        #isRanged = bool
+        self.name = name
+        self.hasMana = hasMana
+        self.isRanged = isRanged
